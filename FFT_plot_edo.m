6a7460f1987459161848cd0b69d5a32cd6bbd592
Edoardo D'Anna <edoardodanna@gmail.com>
2013-09-25 23:47:33 +0200

    Adding FFT_plot_edo.m script

---
+++ FFT_plot_edo.m
@@ -0,0 +1,12 @@
+function [] = FFT_plot_edo(signal,Fs)
+%FFT_plot_edo Compute and plot the FFT given a signal and its sampling
+%frequency.
+%   Detailed explanation goes here
+    signal2=fft(signal);
+    signal2=ifftshift(abs(signal2));
+    a=-1*(Fs/2)+Fs/length(signal);
+    b=Fs/2;
+    axis=[a:Fs/length(signal):b];
+    plot(axis,signal2);
+end
+
