fe9351da72077b526228a16aeb4e15bd813407b6
Edrosos <edoardodanna@gmail.com>
2013-09-26 15:50:55 +0200

    renamed file (removed unrelated mention of EEG), edited README.md and commented plot_fft.m

--- README.md
+++ README.md
@@ -12,4 +12,13 @@ Code by Edoardo D'Anna and Charles Vila.
 
 ### Functions
 - **plot_fft.m**: a simple (but useful) function which allows any sampled signal to be fft'd and ploted.
+
 - **original_plot_from_serial.m**: messy file which needs to be fixed which reads from serial, ffts and plots.
+
+- **classify_edo.m**: simple classifier. Still old script. Needs to be gone through.
+
+- **extract_frequency_features.m**: script to extract features based on breaking down the signal into frequency components. Still old script. Needs to be gone through.
+
+- **plot_live_signal.m**: plots signal received from serial port live . Still old script. Needs to be gone through.
+
+- **plot_power_spectrums.m**: plots the power spectrum of a given signal. Still old script. Needs to be gone through.
--- plot_fft.m
+++ plot_fft.m
@@ -2,19 +2,34 @@ function [] = plot_fft(signal,Fs)
 %plot_fft computes and plots the FFT of a given signal (signal), knowing its sampling rate (Fs).
 %   Simple function displaying the FFT of the input signal.
    
+    %performs the fast fourier transform
     signal2=abs(fft(signal,Fs));
+    %shifts the result so that it is centered around 0 (this is not the
+    %case with the initial output of the previous function)
     signal2=fftshift(abs(signal2));
    
+    %defines the x-axis values corresponding to the fft (the frequency
+    %values)
     axis_values=[-Fs/2:Fs/2-1];
 
+    %plots fft
     plot(axis_values,signal2,'r','LineWidth',1.5);
+    
+    %adjusted the look of the graph
+    
+    %enable tickmarks
     grid on;
     grid minor;
+    
+    %label graph and axes
     title('FFT of input signal');
     xlabel('Frequencies [Hz]');
     ylabel('Amplitude [-]');
     
+    %define range of axes
     axis([(-Fs/2),(Fs/2),0,max(signal2)*1.05]);
+    
+    %set tick marks on x-axis
     set(gca,'XTick',[-1000:10:1000])
 
 end
