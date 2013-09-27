a7f8ff108107867653230e8983c2e528154905e4
charlesvila <charlesvila@tsf-460-wpa-5-108.epfl.ch>
2013-09-27 12:29:44 +0200

    Matlab scripts from charles

---
+++ blink/baseliner.m
@@ -0,0 +1,26 @@
+function [ signal1 signal2 ] = baseliner( signal1, signal2, interval ,draw)
+%BASELINER adjusts the amplitudes of the two signals to the same baseline
+%   interval goes from 1-interval
+sum1=0;
+sum2=0;
+temps1= abs(signal1);
+temps2= abs(signal2);
+
+for i = 1:1:interval
+    sum1 = sum1 + temps1(i);
+    sum2 = sum2 + temps2(i);
+end
+
+baselinediff = sum1/sum2;
+
+signal2 = signal2 * baselinediff;
+if(draw)
+    hold off;
+    figure(2);
+    plot(signal1);
+    hold on;
+    plot(signal2,'r');
+end
+
+end
+
